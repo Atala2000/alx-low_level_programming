4a043bc19bc2555730efa26d5ccf312ac26c4709
Atala2000 <atalakidi@gmail.com>
2023-02-17 08:25:49 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Return: Always (0) is successful
+*/
+int main(void)
+{
+	int chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	putchar(chr);
+	return (0);
+
+}
