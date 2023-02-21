626b320cfbc315bbb4a38f1cc92fa39d90a6e3cd
Atala2000 <atalakidi@gmail.com>
2023-02-21 01:48:57 -0800

    Print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main -Print out putchar
+*Return:0 Successfuly
+*/
+int main(void)
+{
+
+	int s = 95;
+	int p = 112;
+	int u = 117;
+	int c = 116;
+	int h = 99;
+	int q = 104;
+	int r = 97;
+	int t = 114;
+
+	putchar(s);
+	putchar(p);
+	putchar(u);
+	putchar(c);
+	putchar(h);
+	putchar(q);
+	putchar(r);
+	putchar(t);
+	putchar('\n');
+	return (0);
+}
