dc6e56eb6f1c4fb9ab641493e17362cfaf1954ac
Atala2000 <atalakidi@gmail.com>
2023-02-23 09:31:31 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include  "main.h"
+/**
+*more_numbers -prints number 10 times
+*Return: 0 executed
+*/
+void more_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 14; a++)
+	{
+		_putchar(a + '0');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
