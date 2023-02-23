1b090cac8811465fca7c2f9017f82bb5049e322b
Atala2000 <atalakidi@gmail.com>
2023-02-23 08:36:00 -0800

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for ((i >= 48; i <= 57; i++) && (i != 50 && i != 52))
+	for (i = 48; i <= 57; i++) 
+	{
+	if (i != 50 && i != 52)
 	{
 		_putchar(i);
+	}
 	}
 	_putchar('\n');
 }
