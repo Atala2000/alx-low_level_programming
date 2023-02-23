a04de4c5a71b7ffd54ffeef0fa154c3d61e3bb01
Atala2000 <atalakidi@gmail.com>
2023-02-23 08:21:13 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_most_numbers -print numbers
+*Return: 0
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for ((i >= 48; i <= 57; i++) && (i != 50 && i != 52))
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
