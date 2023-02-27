130404249de9e4c7d3b0438d1b3f18f6d274e727
Atala2000 <atalakidi@gmail.com>
2023-02-27 06:39:32 -0800

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*void swap_int
-*@a: First value
-*b: Second value
-Return: 0
+*swap_int -swaps numbers
+*@a:First value
+*@b:Second value
+*Return: 0
 **/
 void swap_int(int *a, int *b)
 {
