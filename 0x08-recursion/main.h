c211421e277329896e5cb1183ba3864d55a91c34
Atala2000 <atalakidi@gmail.com>
2023-03-08 05:27:40 -0800

    correct

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,16 +1,23 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Ephrem Abebe
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x08-recursion
+ */
+ 
+#include <stddef.h>
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-void _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
-int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt(int n, int i);
-int wildcmp(char *s1, char *s2);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
-#endif
+#endif`
