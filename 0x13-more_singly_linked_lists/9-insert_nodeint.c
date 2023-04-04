81c179411fe0f3ad4eb60625b14ee85daa16bf71
Atala2000 <atalakidi@gmail.com>
2023-04-04 12:44:58 -0700

    correction to 9 and 8

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *tmp;
 
-	insigned int x = 0;
+	unsigned int x = 0;
 
 	if (*head == NULL && idx != 0)
 		return (NULL);
