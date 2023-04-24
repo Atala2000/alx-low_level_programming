e6e362b7a8e946f818b4fcbc44da9ee550b91cb2
Atala2000 <atalakidi@gmail.com>
2023-04-23 20:43:45 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main section of code
+ * Return: 0 
+ **/
+int main()
+{
+	int j;
+
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
