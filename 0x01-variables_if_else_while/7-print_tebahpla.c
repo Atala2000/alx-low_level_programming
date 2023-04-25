55961c1e676ff4b7db8f442df76629e55e04dde2
Atala2000 <atalakidi@gmail.com>
2023-04-24 21:56:17 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**main - main section of code
+ *Return: 0 
+ **/
+int main()
+{
+	int j;
+
+	for (j = 'z'; j >= 'a'; j--)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
