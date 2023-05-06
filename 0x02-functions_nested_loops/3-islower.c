97c6804a10cf4d958392fca91410792b7bfb3630
Atala2000 <atalakidi@gmail.com>
2023-05-06 01:31:51 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int _islower - checks if character is lowercase
+ * return: 1 if lower 0 otherwise
+ **/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}	
