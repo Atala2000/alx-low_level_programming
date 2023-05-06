9c8fa744c10c08a02bce307cf5144ef05a866610
Atala2000 <atalakidi@gmail.com>
2023-05-06 00:43:52 -0700

    22

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * void print_alphabet_x10 - prints alphabet 10 times
+ * return: no value
+ **/
+void print_alphabet_x10(void)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (k= 'a'; k <= 'z'; k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+	return;
+}
