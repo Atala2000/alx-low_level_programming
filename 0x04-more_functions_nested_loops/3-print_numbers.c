3d4355eb2c8e0b93a2ca70b889fed3444e313aa8
Atala2000 <atalakidi@gmail.com>
2023-05-07 00:48:47 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - multiplies tow integers
+ * Return: 0
+ **/
+void print_numbers(void)
+{
+	char k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		_putchar(k + '0');
+	}
+	_putchar('\n');
+}
